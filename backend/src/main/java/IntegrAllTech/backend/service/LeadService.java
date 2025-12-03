657947e92e2a654c6e46d83e72e978564cab62cc
Natani Moraes <natanigabriele@gmail.com>
2025-12-02 22:34:14 -0300

    Create LeadService.java

---
+++ backend/src/main/java/IntegrAllTech/backend/service/LeadService.java
@@ -0,0 +1,50 @@
+package IntegrAllTech.backend.service;
+
+import IntegrAllTech.backend.model.Lead;
+import IntegrAllTech.backend.model.StatusLead;
+import IntegrAllTech.backend.repository.LeadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LeadService {
+
+    @Autowired
+    private LeadRepository repository;
+
+//CRIAR NOVO LEAD
+    public Lead salvar(Lead lead){
+        if (lead.getId() == null){
+            lead.setStatus(StatusLead.NOVO);
+        }
+        return repository.save(lead);
+    }
+
+//LISTAR TODOS OS LEADS
+    public List<Lead> listarTodos(){
+        return repository.findAll();
+    }
+
+//BUSCAR LEAD POR ID
+    public Optional<Lead> buscarPorID(Long id){
+        return repository.findById(id);
+    }
+
+//ATUALIZAR STATUS DOS LEADS
+    public Lead atualizarStatus(Long id, StatusLead novoStatus){
+        return repository.findById(id)
+                .map(lead -> {
+            lead.setStatus(novoStatus);
+            lead.setDataAtualizacao(java.time.LocalDateTime.now());
+            return repository.save(lead);
+        }).orElse(null);
+    }
+
+//DELETAR
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
