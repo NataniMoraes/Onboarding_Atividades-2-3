538b6c581569e7ae0e8514f2694722211650f1d1
Natani Moraes <natanigabriele@gmail.com>
2025-12-04 10:16:17 -0300

    Create leadService.ts

---
+++ frontend/src/services/leadService.ts
@@ -0,0 +1,53 @@
+import type { Lead } from "../types/Lead";
+
+const API_URL = 'http://localhost:8080/api/leads';
+
+export const leadService = {
+
+//GET: BUSCAR TODOS OS LEADS
+    async getAll(): Promise<Lead[]>{
+        const response = await fetch(API_URL);
+        if(!response.ok){
+            throw new Error('Erro ao buscar leads');
+        }
+        return response.json();
+    },
+
+//POST: CRIAR NOVO LEAD
+    async create(lead: Omit<Lead, 'id' | 'dataCadastro' | 'dataAtualizacao'>): Promise<Lead> {
+        const response = await fetch(API_URL,{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(lead),
+        });
+
+        if (!response.ok){
+            throw new Error ('Erro ao criar lead');            
+        }        
+        return response.json();
+    },
+
+//PATCH: ATUALIZAR APENAS O STATUS DO LEAD
+    async updateStatus(id: number, status: StatusLead): Promise<Lead> {
+        const response = await fetch(`${API_URL}/${id}/status?status=${status}`,{
+            method: 'PATCH',
+        });
+
+        if (!response.ok){
+            throw new Error('Erro ao atualizar o status');
+        }
+        return response.json();
+    },
+//DELETE: DELETAR UM LEAD
+    async delete (id: number): Promise<void>{
+        const response = await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE'
+        });
+
+        if (!response.ok) {
+        throw new Error('Erro ao deletar o status');
+        }
+    },
+};
